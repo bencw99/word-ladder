23f4206bec6d89a06b40854f3f7042a26aad6211
Edward Lee <edlee1@stanford.edu>
2017-01-26 22:38:10 -0800

    Added more to template

--- src/wordLadder.cpp
+++ src/wordLadder.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <map>
 
+using namespace std;
 // TODO: Everything
+
+// Generate a graph given an input file.
+// Graph of format: Map of string -> vector<string>
+void generateGraph(map<string*, vector<string*>> &graph, string inputFile) {
+
+}
+
+// Gets longest shortest path from begin in graph
+int bfs(map<string*, vector<string*>> &graph, string* begin) {
+
+}
+
 int main(int argc, char* argv[]) {
-	std::cout << "Hello world" << std::endl;
+	cout << "Hello world" << endl;
 }
