e9cfbbe2fd3963c1ee6c351194c9f3ac07c0a0a2
Benjamín <benjaguilarm@gmail.com>
2017-01-26 23:40:24 -0800

    The BFS is here

--- src/wordLadder.cpp
+++ src/wordLadder.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <tuple>
+#include <queue>
 
 using namespace std;
 // TODO: Everything
@@ -13,8 +15,28 @@ void generateGraph(map<string*, vector<string*>> &graph, string inputFile) {
 }
 
 // Gets longest shortest path from begin in graph
-int bfs(map<string*, vector<string*>> &graph, string* begin) {
-
+tuple<int, int, int > bfs(vector<vector<int > > &graph) {
+    int c = 0, first, second, maxx = -1;
+    for(int i = 0; i < graph.size(); i++){
+        queue<int > q;
+        vector<int > distance (graph.size(), 0);
+        q.push(i);
+        while(!q.empty()){
+            int current = q.front(); q.pop();
+            c++;
+            for(int j = 0; j < graph[current].size(); j++){
+                if(distance[graph[current][j]] == 0){
+                    q.push(graph[current][j]);
+                    distance[graph[current][j]] = c;
+                    if(c > maxx){
+                        maxx = c; first = i; second = graph[current][j];
+                    }
+                }
+            }
+        }
+    }
+    tuple<int, int, int > WeDidIt(c, first, second);
+    return WeDidIt;
 }
 
 int main(int argc, char* argv[]) {
