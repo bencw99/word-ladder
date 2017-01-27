8145971801207bee8d239d600b5ed2fa568f6f09
Benjamin Cohen-Wang <ben.cohenwang@gmail.com>
2017-01-27 00:02:39 -0800

    BCW: Completed generate graph

--- src/wordLadder.cpp
+++ src/wordLadder.cpp
@@ -4,17 +4,44 @@
 #include <map>
 
 using namespace std;
-// TODO: Everything
 
-// Generate a graph given an input file.
-// Graph of format: Map of string -> vector<string>
-void generateGraph(map<string*, vector<string*>> &graph, string inputFile) {
+// Checks if the two given words are neighbors
+bool areNeighbors(const string &start, const string &finish) {
+	int diffCount = 0;
+	for(int i = 0; i < start.size(); i ++) {
+		if(start[i] != finish[i]) {
+			diffCount ++;
+		}
+		if(diffCount > 1) {
+			return false;
+		}
+	}
+	if(diffCount == 1) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 
+// Generates a word ladder graph from the given dictionary of words
+void generateGraph(vector<vector<int>> &graph, map<string, int> &dictionaryMap, const vector<string> &dictionary) {
+	for(int i = 0; i < dictionary.size(); i ++) {
+		dictionaryMap[dictionary[i]] = i;
+	}
+	graph.resize(dictionary.size(), vector<int>());
+	for(int i = 0; i < dictionary.size(); i ++) {
+		for(int j = i; j < dictionary.size(); j ++) {
+			if(areNeighbors(dictionary[i], dictionary[j])) {
+				graph[i].push_back(j);
+				graph[j].push_back(i);
+			}
+		}
+	}
 }
 
 // Gets longest shortest path from begin in graph
 int bfs(map<string*, vector<string*>> &graph, string* begin) {
-
 }
 
 int main(int argc, char* argv[]) {
